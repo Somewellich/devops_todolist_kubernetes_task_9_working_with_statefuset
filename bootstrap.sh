0d010e8d9b63276856cc490fce96c807119b4b22
Illia Fedoruk <fedorukstar@gmail.com>
2026-07-31 01:27:39 +0300

    fix: updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+kind create cluster --config cluster.yml
+kubectl create ns todoapp
+kubectl create ns mysql
 kubectl apply -f .infrastructure/service.yml
 kubectl apply -f .infrastructure/configMapDB.yml
 kubectl apply -f .infrastructure/secretDB.yml
 kubectl apply -f .infrastructure/statefulSet.yml
-kubectl apply -f .infrastructure/configMap.yml
 kubectl apply -f .infrastructure/secret.yml
-kubectl apply -f .infrastructure/pv.yml
-kubectl apply -f .infrastructure/pvc.yml
 kubectl apply -f .infrastructure/deployment.yml
